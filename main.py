bb3bbfb5ca3cd79df3125f2018018fb57fd8b87f
Ronit gujjar <92150685+Ronit-gurjar@users.noreply.github.com>
2022-10-27 22:20:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.core.window import Window
-import math
 
 #size of screen
 Window.size = (320,650)
